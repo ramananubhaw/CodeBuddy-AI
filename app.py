041e99935a3f1f40f7cd6be9490bcc3c8e9d58d0
ramananubhaw <anubhawraman@gmail.com>
2025-07-05 22:10:05 +0530

    Model connected

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
-# from model import explain_code
 from model import stream_explanation
 
 st.set_page_config(page_title="CodeBuddy AI", layout="wide")
 
+# st.markdown("""
+#     <style>
+#     textarea[disabled] {
+#         color: white;
+#         font-weight: 500;
+#     }
+#     </style>
+# """, unsafe_allow_html=True)
+
 if "generating" not in st.session_state:
     st.session_state.generating = False
+
+if "finished" not in st.session_state:
+    st.session_state.finished = False
 
 def start_explanation():
     st.session_state.generating = True
@@ -17,13 +28,18 @@ col1, spacer, col2 = st.columns([1, 0.1, 1])
 
 with col1:
     st.markdown("### Input Code")
-    code_input = st.text_area("Your code", height=400, placeholder="Write here....")
+    code_input = st.text_area("Your code", height=400, placeholder="Paste here....")
 
 with col2:
-    # Button to trigger explanation
-    explain = st.button("Explain Code", disabled=st.session_state.generating, on_click=start_explanation)
+    col_btn1, col_btn2, space_btn = st.columns([1, 1, 2.5])
+    with col_btn1:
+        explain_button = st.button("Explain Code", disabled=st.session_state.generating, on_click=start_explanation)
+    with col_btn2:
+        refresh_button = st.button("Refresh")
+        if refresh_button:
+            st.rerun()
 
-    if explain:
+    if st.session_state.generating:
         if not code_input.strip():
             st.warning("Please enter some code.")
             st.session_state.generating = False
@@ -44,4 +60,4 @@ with col2:
 
             st.session_state.generating = False
     else:
-        st.info("Enter code on the left and click on the button.")
+        st.info("Paste code on the left and click on the 'Explain Code' button.")
--- model.py
+++ model.py
@@ -1,15 +1,36 @@
 import time
 import threading
+import os
+from dotenv import load_dotenv
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai import Credentials
+
+load_dotenv()
+
+creds = Credentials(
+    api_key = os.getenv("IBM_API_KEY"),
+    url = os.getenv("SERVICE_URL")
+)
+
+model = ModelInference(
+    model_id = "ibm/granite-3-3-8b-instruct",
+    credentials = creds,
+    project_id = os.getenv("PROJECT_ID"),
+    params = {
+        "max_new_tokens": 1000
+    }
+)
 
 result = { "ready": False, "text": "" }
 
 def explain_code(code: str):
-    # for i in range(100000):
-    #     pass
-    result["text"] = f"Nice code:\n{code}"
+    response = model.generate(prompt=f"Assume that you are a software engineer, not an AI. Explain the following code line-by-line:\n{code}")
+    # print(response["results"][0])
+    result["text"] = response["results"][0]["generated_text"][2:]
     result["ready"] = True
 
 def stream_explanation(code: str):
+    print("Generating..........")
     thread = threading.Thread(target=explain_code, args=(code, ))
     thread.start()
 
@@ -18,13 +39,13 @@ def stream_explanation(code: str):
         time.sleep(0.05)
 
     explanation = result["text"]
+    # print(explanation)
     lines = explanation.splitlines()
     for line in lines:
         for c in line:
             yield c
         yield "\n"
         time.sleep(0.1)
-    # yield "END-OF-EXPLANATION"
 
     result["ready"] = False
     result["text"] = ""
